2a4231d1fe2c1cf331e9f184cb97409c67e3fd6a
SCMkhwanazi <160224688+SCMkhwanazi@users.noreply.github.com>
2025-05-12 23:09:59 +0200

    Create invalidInput.js
    added an NumberFormateException

---
+++ StudentFinalMarkWebApp/web/invalidInput.js
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : invalidInput
+    Created on : 12 May 2025, 7:47:55 PM
+    Author     : sands
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Invalid Input Page</title>
+    </head>
+    <body>
+        <h1>Error:Invalid Input!</h1>
+        <p>
+            An invalid input has been entered.
+            <br>
+            Click <a href="index.html">here</a> to try again.
+            
+        </p>
+    </body>
+</html>
